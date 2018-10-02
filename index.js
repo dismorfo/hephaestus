9bd0044fb82220b80b69784eabeb576fbe6af951
Alberto Ortiz Flores <dismorfo@gmail.com>
2018-10-02 14:23:41 -0400

    updates

--- index.js
+++ index.js
@@ -6,48 +6,41 @@
  * Licensed under the MIT license.
  */
 
-'use strict'
+'use strict';
 
 const agartha = () => {
-
-  // later: - https://github.com/winstonjs/winston
-  //        - https://github.com/foreverjs/forever
-  //        - https://github.com/indexzero/nconf
-
   // http://underscorejs.org
-  const _ = require('underscore')
+  const _ = require('underscore');
   if (!_.isObject(process.agartha)) {
     // https://nodejs.org/api/fs.html
-    const fs = require('fs')
+    const fs = require('fs');
     // https://npmjs.org/package/rimraf
-    const rimraf = require('rimraf')
+    const rimraf = require('rimraf');
     // https://nodejs.org/api/path.html
-    const path = require('path')
+    const path = require('path');
     // https://nodejs.org/api/events.html
-    const events = require('events')
+    const events = require('events');
     // Create an eventEmitter object
-    const eventEmitter = new events.EventEmitter()
+    const eventEmitter = new events.EventEmitter();
     // https://github.com/request/request
-    const request = require('request')
+    const request = require('request');
     // https://github.com/felixge/node-dateformat
-    const dateformat = require('dateformat')
+    const dateformat = require('dateformat');
     // track start time
-    const timespan = _.now()
+    const timespan = _.now();
     // https://github.com/substack/node-mkdirp
-    const mkdirp = require('mkdirp')
+    const mkdirp = require('mkdirp');
     // https://github.com/adam-lynch/graceful-ncp
     // https://github.com/AvianFlu/ncp
-    let copy = require('ncp').ncp
-        copy.limit = 16
-    // https://github.com/mikaelbr/node-notifier
-    const notifier = require('node-notifier')
+    let copy = require('ncp').ncp;
+    copy.limit = 16;
     // https://www.npmjs.com/package/node-cleanup
-    const nodeCleanup = require('node-cleanup')
+    const nodeCleanup = require('node-cleanup');
 
-    let modules = {}
+    let modules = {};
 
     fs.readdirSync(path.join(__dirname, 'lib')).forEach((module) => {
-      modules[module] = require(path.join(__dirname, 'lib', module))
+      modules[module] = require(path.join(__dirname, 'lib', module));
     })
 
     process.agartha = {
@@ -64,24 +57,25 @@ const agartha = () => {
       notifier: notifier,
       _onInit: [],
       _onDone: [],
+      _onForge: [],
       _filemap: {}
-    }
+    };
 
-    _.extendOwn(process.agartha, modules)
+    _.extendOwn(process.agartha, modules);
 
     nodeCleanup((exitCode, signal) => {
       process.agartha._.forEach(process.agartha._onDone, (callback) => {
-        process.agartha.log('Callback triggered by ' + callback.command + ' onDone', 'signal')
+        process.agartha.log('Callback triggered by ' + callback.command + ' onDone', 'signal');
         if (process.agartha._.isFunction(callback.action)) {
-          callback.action()
+          callback.action();
         }
       })
     })
 
   }
 
-  return process.agartha
+  return process.agartha;
 
 }
 
-exports.agartha = agartha()
+exports.agartha = agartha();
