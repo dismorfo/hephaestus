56c620adb77d5dd7ae93071d4a2dfc7fdce60bee
Alberto Ortiz Flores <dismorfo@gmail.com>
2018-11-06 13:19:34 -0500

    updates

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 /**
- * agartha.js
- * https://github.com/NYULibraries/agartha.js
- *
- * Copyright (c) 2014 New York University, contributors
- * Licensed under the MIT license.
+ * hephaestus.js
  */
-
 'use strict';
-
 const agartha = () => {
   // http://underscorejs.org
   const _ = require('underscore');
@@ -19,9 +13,9 @@ const agartha = () => {
     // https://nodejs.org/api/path.html
     const path = require('path');
     // https://nodejs.org/api/events.html
-    const events = require('events');
+    // const events = require('events');
     // Create an eventEmitter object
-    const eventEmitter = new events.EventEmitter();
+    // const eventEmitter = new events.EventEmitter();
     // https://github.com/request/request
     const request = require('request');
     // https://github.com/felixge/node-dateformat
@@ -30,7 +24,6 @@ const agartha = () => {
     const timespan = _.now();
     // https://github.com/substack/node-mkdirp
     const mkdirp = require('mkdirp');
-    // https://github.com/adam-lynch/graceful-ncp
     // https://github.com/AvianFlu/ncp
     let copy = require('ncp').ncp;
     copy.limit = 16;
@@ -40,48 +33,39 @@ const agartha = () => {
     const notifier = require('node-notifier');
     // https://www.npmjs.com/package/solr-client
     const discovery = require('solr-client');
-
     let modules = {};
-
     fs.readdirSync(path.join(__dirname, 'lib')).forEach((module) => {
       modules[module] = require(path.join(__dirname, 'lib', module));
     })
-
     process.agartha = {
       _: _,
+      fs: fs,
       path: path,
       timespan: timespan,
       request: request,
-      on: eventEmitter.addListener,
-      emit: eventEmitter.emit,
       dateformat: dateformat,
       copy: copy,
       discovery: discovery,
-      nodeCleanup : nodeCleanup,
-      fs: fs,
       rimraf: rimraf,
       notifier: notifier,
-      _onInit: [],
-      _onDone: [],
-      _onForge: [],
-      _filemap: {}
+      _onInit: [], // will remove and find a better solution
+      _onDone: [], // will remove and find a better solution
+      _onForge: [], // will remove and find a better solution
+      _filemap: {} // will remove and find a better solution. Am I using this?
     };
-
     _.extendOwn(process.agartha, modules);
-
-    nodeCleanup((exitCode, signal) => {
+    // new module?
+    nodeCleanup(() => {
       process.agartha._.forEach(process.agartha._onDone, (callback) => {
         process.agartha.log('Callback triggered by ' + callback.command + ' onDone', 'signal');
         if (process.agartha._.isFunction(callback.action)) {
           callback.action();
         }
-      })
-    })
+      });
+    });
 
   }
-
   return process.agartha;
-
 }
 
 exports.agartha = agartha();
