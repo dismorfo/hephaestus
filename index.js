25b706ca0193f482ad42dd2ed450880a817ddf69
Alberto Ortiz Flores <aortiz@nyu.edu>
2019-11-05 08:35:06 -0500

    updates

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
 /**
  * hephaestus.js
  */
-'use strict';
 
-// http://underscorejs.org
+ // http://underscorejs.org
 const _ = require('underscore');
+
 // https://nodejs.org/api/fs.html
-const fs = require('fs');
-// https://npmjs.org/package/rimraf
-const rimraf = require('rimraf');
+const {
+  readdirSync
+} = require('fs');
+
 // https://nodejs.org/api/path.html
-const { resolve } = require('path');
-// https://github.com/felixge/node-dateformat
-const dateformat = require('dateformat');
-// track start time
-const timespan = _.now();
-// https://github.com/AvianFlu/ncp
-let copy = require('ncp').ncp;
-copy.limit = 16;
-// https://www.npmjs.com/package/node-cleanup
-const nodeCleanup = require('node-cleanup');
-// https://www.npmjs.com/package/node-notifier
-const notifier = require('node-notifier');
-// https://www.npmjs.com/package/solr-node
-const discovery = require('solr-node');
+const {
+  resolve 
+} = require('path');
 
 let modules = {};
 
-fs.readdirSync(resolve(__dirname, 'lib')).forEach(library => {
+readdirSync(resolve(__dirname, 'lib')).forEach(library => {
   modules[library] = require(resolve(__dirname, 'lib', library));
 });
 
 module.exports = _.extendOwn({
-  timespan: timespan,
-  dateformat: dateformat,
-  copy: copy,
-  discovery: discovery,
-  rimraf: rimraf,
-  notifier: notifier,
-  nodeCleanup: nodeCleanup
+  timespan: _.now(),
+  dateformat: require('dateformat'), // https://github.com/felixge/node-dateformat
+  copy: require('ncp').ncp, // https://github.com/AvianFlu/ncps
+  discovery: require('solr-node'), // https://www.npmjs.com/package/solr-node
+  rimraf: require('rimraf'), // https://npmjs.org/package/rimraf
+  notifier: require('node-notifier'), // https://www.npmjs.com/package/node-notifier
+  nodeCleanup: require('node-cleanup'), // https://www.npmjs.com/package/node-cleanup
 }, modules);
+
